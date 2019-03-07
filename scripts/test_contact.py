273eb3a5868401cfb3fa9c6f3eac88d539a10e52
273souffle <15811593542@163.com>
2019-03-07 22:09:26 +0800

    base po

---
+++ scripts/test_contact.py
@@ -0,0 +1,34 @@
+import allure
+import pytest
+
+from base.base_analyze import analyze_file
+from base.base_driver import init_driver
+from page.page import Page
+
+
+class TestContact:
+
+    def setup(self):
+        self.driver = init_driver()
+        self.page = Page(self.driver)
+
+    def teardown(self):
+        self.driver.quit()
+
+    @pytest.mark.parametrize(("name", "phone"), analyze_file("contact_data", "test_add_contact"))
+    def test_add_contact(self, name, phone):
+        self.page.contact_list.click_add_contact()
+        self.page.add_contact.input_name(name)
+        self.page.add_contact.input_phone(phone)
+        # 截图添加到报告
+        self.driver.get_screenshot_as_file("./screen/xx.png")
+        # 添加到报告上
+        with open("./screen/xx.png", 'rb') as f:
+            allure.attach('描述', f.read(), allure.attach_type.PNG)
+        # allure.attach('描述', self.driver.get_screenshot_as_png(), allure.attach_type.PNG)
+
+    @pytest.mark.parametrize(("nickname", "phone"), analyze_file("contact_data", "test_add_nickname"))
+    def test_add_nickname(self, nickname, phone):
+        self.page.contact_list.click_add_contact()
+        self.page.add_contact.input_nickname(nickname)
+        self.page.add_contact.input_phone(phone)
